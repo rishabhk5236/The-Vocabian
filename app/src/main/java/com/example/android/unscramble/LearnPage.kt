17b449a6604086aa2bf65e658eff63cee935e8b9
Rishabh <rishabhk5236@gmail.com>
2022-10-27 12:06:45 +0530

    First Commit

---
+++ app/src/main/java/com/example/android/unscramble/LearnPage.kt
@@ -0,0 +1,41 @@
+package com.example.android.unscramble
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.unscramble.databinding.ActivityLearnPageBinding
+import com.example.android.unscramble.ui.learn.LetterAdapter
+
+class LearnPage : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityLearnPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Get the navigation host fragment from this Activity
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        // Instantiate the navController using the NavHostFragment
+        navController = navHostFragment.navController
+        // Make sure actions in the ActionBar get propagated to the NavController
+        setupActionBarWithNavController(navController)
+    }
+
+    /**
+     * Enables back button support. Simply navigates one element up on the stack.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/com/example/android/unscramble/frontPage.kt
@@ -0,0 +1,5 @@
+package com.example.android.unscramble
+
+class frontPage {
+
+}
